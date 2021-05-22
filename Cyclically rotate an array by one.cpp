a012270db9c792e9250c9431f29bff5155b2fb82
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-05-22 20:56:27 +0530

    Update Cyclically rotate an array by one.cpp

--- Cyclically rotate an array by one.cpp
+++ Cyclically rotate an array by one.cpp
@@ -4,6 +4,9 @@ void swap(int* first,int* second)
     *first=*second;
     *second=temp;
 }
+
+//Approach 1
+/*--------------------------------------------------*/
 void rot(int arr[],int start,int end)
 {
     int left=start;
@@ -20,4 +23,14 @@ void rotate(int arr[], int n)
 {
     rot(arr,0,n-1);
     rot(arr,1,n-1);
+}
+
+//Approach 2
+/*--------------------------------------------------*/
+void rotate(int arr[], int n)
+{
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;i--)
+        arr[i]=arr[i-1];
+    arr[0]=temp;
 }
