e6f38a0ad5b5fd76ee52923b67638e70f477b577
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-06-17 19:34:43 +0530

    Update Spiral Traversal on a Matrix.cpp

--- Spiral Traversal on a Matrix.cpp
+++ Spiral Traversal on a Matrix.cpp
@@ -1,3 +1,4 @@
+//Approach 1
 typedef long long ll;
 class Solution {
 public:
@@ -32,6 +33,64 @@ public:
             {
                 for(int i=b;i>=a;i--)
                     ans.push_back(matrix[i][c]);
+                c++;
+            }
+            
+            dir=(dir+1)%4;
+        }
+        return ans;
+    }
+};
+
+
+//Approach 2
+typedef long long ll;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int row=matrix.size();
+        int col=matrix.at(0).size();
+        //0->right, 1->down, 2->left, 3->up ;
+        int dir=0;
+        int a=0,b=row-1,c=0,d=col-1;
+        int count=0;
+        while(count<row*col)
+        {
+            if(dir==0)
+            {
+                for(int i=c;i<=d;i++)
+                {
+                    ans.push_back(matrix[a][i]);
+                    count++;
+                }
+                a++;
+            }
+            else if(dir==1)
+            {
+                for(int i=a;i<=b;i++)
+                {
+                    ans.push_back(matrix[i][d]);
+                    count++;
+                }
+                d--;
+            }
+            else if(dir==2)
+            {
+                for(int i=d;i>=c;i--)
+                {
+                    ans.push_back(matrix[b][i]);
+                    count++;
+                }
+                b--;
+            }
+            else
+            {
+                for(int i=b;i>=a;i--)
+                {
+                    ans.push_back(matrix[i][c]);
+                    count++;
+                }
                 c++;
             }
             
