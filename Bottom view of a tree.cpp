fcab0a78ed84cdf3bc54d0e3b5c7473f35dfcbb3
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-02 00:48:15 +0530

    Update Bottom view of a tree.cpp

--- Bottom view of a tree.cpp
+++ Bottom view of a tree.cpp
@@ -2,7 +2,6 @@ class Solution {
   public:
     vector <int> bottomView(Node *root) {
         vector<int> ans;
-        vector<pair<int,int>> temp;
         map<int,int> levelNode;
         if(!root)
             return ans;
@@ -11,15 +10,13 @@ class Solution {
         while(!q.empty())
         {
             pair<Node*,int> curNode=q.front();
-            temp.push_back({curNode.second,curNode.first->data});
+            levelNode[curNode.second]=curNode.first->data;
             q.pop();
             if(curNode.first->left)
                 q.push({curNode.first->left,curNode.second-1});
             if(curNode.first->right)
                 q.push({curNode.first->right,curNode.second+1});
         }
-        for(int i=temp.size()-1;i>=0;i--)
-            levelNode.insert({temp[i].first,temp[i].second});
         for(auto it=levelNode.begin();it!=levelNode.end();it++)
             ans.push_back(it->second);
         return ans;
