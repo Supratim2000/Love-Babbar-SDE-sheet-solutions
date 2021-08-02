2c89d782acd334bb18bb26e1d752b6f76c21e868
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-02 23:02:37 +0530

    Create Convert Binary Tree into Sum Tree.cpp

---
+++ Convert Binary Tree into Sum Tree.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    void toSumTree(Node *node)
+    {
+        if(!node)
+            return;
+        else if(node->left==nullptr && node->right==nullptr)
+            node->data=0;
+        else
+        {
+            if(node->left && node->right)
+            {
+                int leftVal=node->left->data;
+                int rightVal=node->right->data;
+                toSumTree(node->left);
+                toSumTree(node->right);
+                node->data=leftVal+rightVal+node->left->data+node->right->data;
+            }
+            else if(node->left)
+            {
+                int leftVal=node->left->data;
+                toSumTree(node->left);
+                node->data=node->left->data+leftVal;
+            }
+            else
+            {
+                int rightVal=node->right->data;
+                toSumTree(node->right);
+                node->data=node->right->data+rightVal;
+            }
+        }
+    }
+};
