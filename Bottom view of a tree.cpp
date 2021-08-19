749a7746555453811e3d362585f2465bd06a2ca5
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-19 23:03:54 +0530

    Update Bottom view of a tree.cpp

--- Bottom view of a tree.cpp
+++ Bottom view of a tree.cpp
@@ -27,3 +27,45 @@ class Solution
         return ans;
     }
 };
+
+//Method 2(DFS)
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    
+    void traverse(Node* root,int spreadLevel,int verticalLevel,map<int,pair<int,int>>& spread)
+    {
+        if(root==nullptr)
+            return;
+        
+        if(spread.find(spreadLevel)==spread.end())
+            spread.insert({spreadLevel,{verticalLevel,root->data}});
+        else
+        {
+            if(verticalLevel>=spread[spreadLevel].first)
+            {
+                spread[spreadLevel].second=root->data;
+                spread[spreadLevel].first=verticalLevel;
+            }
+        }
+        traverse(root->left,spreadLevel-1,verticalLevel+1,spread);
+        traverse(root->right,spreadLevel+1,verticalLevel+1,spread);
+    }
+    
+    vector<int> bottomView(Node *root)
+    {
+        vector<int> ans;
+        if(root==nullptr)
+            return ans;
+        //horizontal level, vertical level, value
+        map<int,pair<int,int>> spread;
+        int spreadLevel=0;
+        int verticalLevel=0;
+        traverse(root,spreadLevel,verticalLevel,spread);
+        for(auto x: spread)
+            ans.push_back(x.second.second);
+        return ans;
+    }
+};
