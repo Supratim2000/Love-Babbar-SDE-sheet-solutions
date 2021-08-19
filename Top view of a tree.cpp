08d46d952751085b969894972983af15b8dd9d6b
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-19 22:50:29 +0530

    Update Top view of a tree.cpp

--- Top view of a tree.cpp
+++ Top view of a tree.cpp
@@ -1,3 +1,4 @@
+//Method 1(Using BFS)
 class Solution
 {
     public:
@@ -27,6 +28,48 @@ class Solution
         }
         for(auto it=levelNode.begin();it!=levelNode.end();it++)
             ans.push_back(it->second);
+        return ans;
+    }
+};
+
+//Method 2(Using DFS)
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    
+    void traverse(Node* root,int spreadLevel,int verticalLevel,map<int,pair<int,int>>& spread)
+    {
+        if(root==nullptr)
+            return;
+        
+        if(spread.find(spreadLevel)==spread.end())
+            spread.insert({spreadLevel,{verticalLevel,root->data}});
+        else
+        {
+            if(verticalLevel<spread[spreadLevel].first)
+            {
+                spread[spreadLevel].second=root->data;
+                spread[spreadLevel].first=verticalLevel;
+            }
+        }
+        traverse(root->left,spreadLevel-1,verticalLevel+1,spread);
+        traverse(root->right,spreadLevel+1,verticalLevel+1,spread);
+    }
+    
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        if(root==nullptr)
+            return ans;
+        //horizontal level, vertical level, value
+        map<int,pair<int,int>> spread;
+        int spreadLevel=0;
+        int verticalLevel=0;
+        traverse(root,spreadLevel,verticalLevel,spread);
+        for(auto x: spread)
+            ans.push_back(x.second.second);
         return ans;
     }
 };
