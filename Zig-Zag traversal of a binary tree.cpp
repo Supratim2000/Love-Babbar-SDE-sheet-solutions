39446073e07cd7fca423f74140bd4546c25a3b8e
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-20 00:24:33 +0530

    Update Zig-Zag traversal of a binary tree.cpp

--- Zig-Zag traversal of a binary tree.cpp
+++ Zig-Zag traversal of a binary tree.cpp
@@ -1,3 +1,4 @@
+//Method 1(Using QUEUE and reversing after level interval)
 vector <int> zigZagTraversal(Node* root)
 {
     bool t=true;
@@ -34,3 +35,49 @@ vector <int> zigZagTraversal(Node* root)
 	}
 	return ans;
 }
+
+
+//Method 2(Using DEQUEUE) (Most optimised)
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+        vector<int> ans;
+        if(!root)
+            return ans;
+    	deque<Node*> dq;
+    	int level=1;
+    	dq.push_front(root);
+    	while(!dq.empty())
+    	{
+    	    int n=dq.size();
+    	    for(int i=0;i<n;i++)
+    	    {
+    	        Node* curNode=nullptr;
+    	        if(level&1)
+    	        {
+    	            curNode=dq.back();
+    	            ans.push_back(curNode->data);
+    	            dq.pop_back();
+    	            if(curNode->left)
+    	                dq.push_front(curNode->left);
+    	            if(curNode->right)
+    	                dq.push_front(curNode->right);
+    	        }
+    	        else
+    	        {
+    	            curNode=dq.front();
+    	            ans.push_back(curNode->data);
+    	            dq.pop_front();
+    	            if(curNode->right)
+    	                dq.push_back(curNode->right);
+    	            if(curNode->left)
+    	                dq.push_back(curNode->left);
+    	        }
+    	    }
+    	    level++;
+    	}
+    	return ans;
+    }
+};
