d1dbf54f2c9de660f986630c2bf765a3da4c6a2a
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-19 21:09:49 +0530

    Update Right View Of A Binary Tree.cpp

--- Right View Of A Binary Tree.cpp
+++ Right View Of A Binary Tree.cpp
@@ -30,4 +30,31 @@ class Solution
 };
 
 //Method 1(DFS)
-
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    
+    void traverse(Node *root,int height,int& maxHeightTillNow,vector<int>& view)
+    {
+        if(root==nullptr)
+            return;
+        if(height>maxHeightTillNow)
+        {
+            maxHeightTillNow=height;
+            view.push_back(root->data);
+        }
+        traverse(root->right,height+1,maxHeightTillNow,view);
+        traverse(root->left,height+1,maxHeightTillNow,view);
+    }
+    
+    vector<int> rightView(Node *root)
+    {
+        vector<int> view;
+        int maxHeightTillNow=-1;
+        if(root==nullptr)
+            return view;
+        traverse(root,0,maxHeightTillNow,view);
+        return view;
+    }
+};
