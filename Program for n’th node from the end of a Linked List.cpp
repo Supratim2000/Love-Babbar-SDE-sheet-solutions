32e4202dddf029b94233f5e4bc3121b1b99e1ff5
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-18 19:45:19 +0530

    Update Program for n’th node from the end of a Linked List.cpp

--- Program for n’th node from the end of a Linked List.cpp
+++ Program for n’th node from the end of a Linked List.cpp
@@ -1,3 +1,26 @@
+//Brute force Approach
+//Time complexity:- O(n)(Double pass)
+//Space complexity:- O(1)
+int getNthFromLast(Node *head, int n)
+{
+    Node* p=head;
+    int size=0;
+    while(p)
+    {
+        size++;
+        p=p->next;
+    }
+    p=head;
+    if(n>size)
+        return -1;
+    for(int i=n;i<size;i++)
+        p=p->next;
+    return p->data;
+}
+
+//Efficient Approach
+//Time complexity:- O(n)(Single pass)
+//Space complexity:- O(1)
 int getNthFromLast(Node *head, int n)
 {
     Node* f=head;
