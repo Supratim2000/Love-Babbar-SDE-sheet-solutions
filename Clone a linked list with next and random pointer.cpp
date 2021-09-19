871bbc8ac63b5baa376d8ed1e42e08ddb12a3b3f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-19 19:38:19 +0530

    Update Clone a linked list with next and random pointer.cpp

--- Clone a linked list with next and random pointer.cpp
+++ Clone a linked list with next and random pointer.cpp
@@ -1,4 +1,4 @@
-//Unoptimised
+//Unoptimised(Using hashing)
 //Time Complexity:- O(N)
 //Space Complexity:- O(N)
 class solution
@@ -29,5 +29,52 @@ class solution
             t=t->next;
         }
         return randMp[head];
+    }
+};
+
+//Optimised
+//Time Complexity:- O(N)
+//Space Complexity:- O(1)
+class solution
+{
+    public:
+    Node *copyList(Node *head)
+    {
+        Node* t=head;
+        Node* dummy=new Node(0);
+        Node* prev=dummy;
+        Node* ans=dummy;
+        Node* p=head;
+        Node* q=head->next;
+        while(p)
+        {
+            Node* temp=new Node(p->data);
+            p->next=temp;
+            temp->next=q;
+            p=p->next->next;
+            if(q)
+                q=q->next;
+        }
+        while(t)
+        {
+            if(t->arb)
+                t->next->arb=t->arb->next;
+            t=t->next->next;
+        }
+        p=head;
+        q=head->next->next;
+        while(p)
+        {
+            prev->next=p->next;
+            prev=p->next;
+            prev->next=nullptr;
+            p->next=q;
+            p=q;
+            if(q)
+                q=q->next->next;
+        }
+        ans=ans->next;
+        delete dummy;
+        return ans;
     }
 };
