f25f8c3e12e7aa69bcbb5e52b2e425ba4e7467f2
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-10-03 21:58:24 +0530

    Update Count triplet with sum smaller than a given value.cpp

--- Count triplet with sum smaller than a given value.cpp
+++ Count triplet with sum smaller than a given value.cpp
@@ -1,18 +1,16 @@
-class Solution{	
-  public:
+class Solution{
+	public:
 	long long countTriplets(long long arr[], int n, long long sum)
 	{
 	    int count=0;
 	    sort(arr,arr+n);
 	    for(int i=0;i<=n-3;i++)
 	    {
-	        int c=i;
 	        int l=i+1;
 	        int h=n-1;
 	        while(l<h)
 	        {
-	            int s=arr[c]+arr[l]+arr[h];
-	            if(s<sum)
+	            if(arr[i]+arr[l]<sum-arr[h])
 	            {
 	                count+=h-l;
 	                l++;
