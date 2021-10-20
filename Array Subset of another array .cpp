4e68c53d8ad23d27cd8dcc42c82b800d64b93f01
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-10-21 00:10:42 +0530

    Update Array Subset of another array .cpp

--- Array Subset of another array .cpp
+++ Array Subset of another array .cpp
@@ -18,7 +18,6 @@ bool binSrc(int arr[],int n,int key)
 
 string isSubset(int a1[], int a2[], int n, int m) {
     sort(a1,a1+n);
-    sort(a2,a2+m);
     for(int i=0;i<m;i++)
     {
         if(!binSrc(a1,n,a2[i]))
